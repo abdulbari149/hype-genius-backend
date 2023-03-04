5b33c07a462adcbc7666f0e3e641f14413749812
abdulbari149 <abdulbari131103@gmail.com>
2023-03-05 03:03:49 +0500

    app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.resolve(ConfigService);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.setGlobalPrefix(config.get('app.prefix'));
+  await app.listen(config.get('app.port'));
 }
 bootstrap();
