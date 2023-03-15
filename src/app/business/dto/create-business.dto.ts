c3834345f25a09f5b9b8eaccbbc97f9d41b8256c
abdulbari149 <abdulbari131103@gmail.com>
2023-03-15 22:35:42 +0500

    feat: create business dto

--- src/app/business/dto/create-business.dto.ts
+++ src/app/business/dto/create-business.dto.ts
@@ -1,1 +1,12 @@
-export class CreateBusinessDto {}
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
+export class CreateBusinessDto {
+  @IsNotEmpty()
+  @IsString()
+  public name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
+  public link: string;
+}
