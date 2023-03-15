ae92343c54f51c5cc8f702e040eeb179b175862d
abdulbari149 <abdulbari131103@gmail.com>
2023-03-15 22:36:29 +0500

    feat: create business service implementation

--- src/app/business/business.service.ts
+++ src/app/business/business.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import BusinessEntity from './entities/business.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateBusinessDto } from './dto/create-business.dto';
 
 @Injectable()
 export default class BusinessService {
@@ -9,4 +11,15 @@ export default class BusinessService {
     @InjectRepository(BusinessEntity)
     private businessRepository: Repository<BusinessEntity>,
   ) {}
+
+  public async createBusiness(data: CreateBusinessDto, adminId: number) {
+    const businessExists = await this.businessRepository.findOne({
+      where: { name: data.name },
+    });
+    if (!businessExists) {
+      throw new ConflictException('Business already exists');
+    }
+    const business = plainToInstance(BusinessEntity, { ...data, adminId });
+    return await this.businessRepository.save(business);
+  }
 }
